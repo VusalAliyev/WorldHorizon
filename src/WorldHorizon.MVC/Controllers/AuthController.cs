92a22cd21bf79a2b52582bf5d653438da857b9a2
Vusal Aliyev <vusalaliev02@gmail.com>
2023-07-23 16:59:35 +0400

    Add project files.

---
+++ src/WorldHorizon.MVC/Controllers/AuthController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WorldHorizon.MVC.Controllers
+{
+	public class AuthController : Controller
+	{
+		public IActionResult Login()
+		{
+			return View();
+		}
+		public IActionResult Register()
+		{
+			return View();
+		}
+		public IActionResult ForgotPassword()
+		{
+			return View();
+		}
+		public IActionResult VerifyOTP()
+		{
+			return View();
+		}
+		public IActionResult ResetPassword()
+		{
+			return View();
+		}
+		public IActionResult Yoxlama()
+		{
+			return View();
+		}
+	}
+}
---
+++ src/WorldHorizon.MVC/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WorldHorizon.MVC.Models;
+
+namespace WorldHorizon.MVC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
