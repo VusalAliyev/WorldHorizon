6daaa434080e35ec42768bf95e9d66345ac6a6f5
Vusal Aliyev <vusalaliev02@gmail.com>
2023-07-24 17:15:06 +0400

    Flight logic started

---
+++ src/WorldHorizon.Application/ServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldHorizon.Application
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ src/WorldHorizon.Infrastructure/ServiceConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorldHorizon.Infrastructure
+{
+    public static class ServiceConfiguration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(configuration.GetConnectionString("DbConnection")));
+            return services;
+        }
+    }
+}
